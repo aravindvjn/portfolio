a3380799de1896e8e0807fc9cdf7996d7be27c32
aravind <aravind284479@gmail.com>
2025-11-26 16:21:21 +0530

    feat : profile changed

--- src/components/about-me/photo.tsx
+++ src/components/about-me/photo.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import me from '@/assets/me.png'
-import Image from 'next/image'
 
 const Photo = () => {
   return (
     <div className='w-[100px] sm:min-w-[150px] md:min-w-[200px] lg:min-w-[250px]'>
-      <Image src={me} alt="Aravind Vijayan's profile picture"  />
+      <img src='/image.png' loading='eager' alt="Aravind Vijayan's profile picture"  />
     </div>
   )
 }
