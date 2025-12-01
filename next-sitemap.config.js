ad07761522efc6c07cb11571dfb31605dc2b2d8d
Aravind Vijayan <aravind284479@gmail.com>
2025-12-02 01:08:03 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://aravindvijayan.in",
+    siteUrl: "https://aravindvijayan.vercel.app",
     generateRobotsTxt: true, 
   };
   
